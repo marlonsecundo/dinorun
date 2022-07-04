e33f23fc63cbc7e0d79c77177e31871ce09bfbc3
Marlon Secundo <marlon_secundo@outlook.com>
2022-07-04 16:04:34 -0300

    feat: run effect]

---
+++ player.gd
@@ -0,0 +1,35 @@
+extends KinematicBody2D
+
+
+var gravity = 30;
+var velocity = Vector2();
+var speed = Vector2(0, 750);
+var slowdown = Vector2(20, 0);
+var acceleration = Vector2(0,0)
+var max_acceleration = Vector2(450,0);
+var speed_acce = 2;
+
+
+func _ready():
+	
+	pass
+	
+	
+
+func _physics_process(delta):
+	velocity.y += gravity;
+	
+	velocity = move_and_slide(velocity, Vector2.UP);
+	
+	
+
+	
+	_handle_input();
+	
+
+
+func _handle_input():
+		
+	if Input.is_action_just_pressed("ui_up") && is_on_floor():
+		velocity.y = -speed.y;
+		
